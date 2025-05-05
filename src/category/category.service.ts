c6496862d9a248abcb19504c059e365150deab54
ikostenevich <ikostenevich.dev@gmail.com>
2025-05-05 23:23:44 +0300

    fix: remove conversion option

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -15,8 +15,6 @@ export class CategoryService {
       },
     })
 
-    return plainToInstance(CategoryResponse, categories, {
-      enableImplicitConversion: true,
-    })
+    return plainToInstance(CategoryResponse, categories)
   }
 }
