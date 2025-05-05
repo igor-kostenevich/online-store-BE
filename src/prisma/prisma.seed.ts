e86c56bc98239aed784b73e3385b9179e0767724
ikostenevich <ikostenevich.dev@gmail.com>
2025-05-05 16:13:39 +0300

    feat: add users seeds

---
+++ src/prisma/prisma.seed.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Logger } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
+import { getUsers } from './seed-data/users'
+
+async function main() {
+  try {
+    Logger.log('Seeding database...')
+    
+    await prisma.$transaction([
+      prisma.user.deleteMany(),
+    ])
+
+    const users = await getUsers()
+    await prisma.user.createMany({
+      data: users,
+    })
+    Logger.log(`Seeded ${users.length} users`)
+
+    
+  } catch (error) {
+    Logger.log(error)
+    throw new BadRequestException('Error seeding database')
+  } finally {
+    Logger.log('Closing DB connection...')
+    await prisma.$disconnect()
+    Logger.log('DB connection closed')
+  }
+}
+
+
+main()
